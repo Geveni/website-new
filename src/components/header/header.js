91745ec0e3ca129ba6571da797f2cdfacb416d92
Cherie Kong <c.kong@mail.utoronto.ca>
2022-01-14 23:30:56 -0500

    added new files

---
+++ src/components/header/header.js
@@ -0,0 +1,14 @@
+import {React} from 'react';
+import {ButtonGroup, Button} from '@mui/material'; 
+
+function Header() {
+  return (
+    <div>
+      <ButtonGroup>
+        <Button> boop </Button>
+      </ButtonGroup>
+    </div>
+  );
+}
+
+export default Header; 
